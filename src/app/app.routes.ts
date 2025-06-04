c288ee02e5655caeb138a5b6659308da196c5e80
Anderson Betancourt Arenas <111938729+lABetancourtl@users.noreply.github.com>
2025-06-03 21:24:36 -0500

    Pagina login
    esta pagina solo es para administradores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
+import { LoginComponent } from './admin/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' }, 
     { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: 'home' }
 ];
