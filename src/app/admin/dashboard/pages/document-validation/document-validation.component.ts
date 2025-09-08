e397f446a796ce4f31d93cf04b9792eb22ced297
Anderson Betancourt Arenas <111938729+lABetancourtl@users.noreply.github.com>
2025-09-07 22:01:03 -0500

    front

--- src/app/admin/dashboard/pages/document-validation/document-validation.component.ts
+++ src/app/admin/dashboard/pages/document-validation/document-validation.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Modal } from 'bootstrap';
+import { AdminService } from '../../../../services/admin.service';
 
 @Component({
   selector: 'app-document-validation',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './document-validation.component.html',
   styleUrl: './document-validation.component.css'
 })
-export class DocumentValidationComponent {
+export class DocumentValidationComponent implements AfterViewInit, OnInit{
+
+
+  usersEmailVerificado: any[] = [];
+  selectedEmail: string = '';
+
+  constructor(
+    private adminService: AdminService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.adminService.getUsuarioEmailVerificado().subscribe({
+      next: (data) => {
+        console.log('Usuarios con email verificado:', data);
+        this.usersEmailVerificado = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener los usuarios:', err);
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+abrirModalConEmail(email: string): void {
+  this.selectedEmail = email;
+  const modalElement = document.getElementById('miModal');
+  if (modalElement) {
+    // @ts-ignore
+    const bsModal = new window.bootstrap.Modal(modalElement);
+    bsModal.show();
+  }
+}
 
 }
---
+++ src/app/public/validation-email/validation-email.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-validation-email',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './validation-email.component.html',
+  styleUrls: ['./validation-email.component.css']
+})
+export class ValidationEmailComponent {
+
+  email: string = '';
+  codigoValidacion: string = '';
+  showErrorAlert: boolean = false;
+  errorMsg: any;
+
+  constructor(
+    private loginService: LoginService,
+    private snackBar: MatSnackBar,
+  ) {}  
+
+  validarEmail() {
+    if (!this.email || !this.codigoValidacion) {
+      this.errorMsg = 'Por favor completa todos los campos requeridos.';
+      return;
+    }
+
+    const datosActivacion = {
+      codigoValidacion: this.codigoValidacion,
+      email: this.email
+    };
+
+
+
+    this.loginService.validarEmail(datosActivacion).subscribe({
+      next: (response) => {
+
+        console.log('Respuesta del servidor:', response);
+        if(response.error === false) {
+        this.snackBar.open('Cuenta activada correctamente', 'Cerrar', { duration: 3000 });
+        window.location.href = '/home';
+        }
+        else {
+          this.errorMsg = response.mensaje || 'Error al validar el email. Por favor, intenta de nuevo.';
+        }
+      },
+      error: (error: any) => {
+        console.error('Error al validar el email:', error);
+        this.errorMsg = error.error?.mensaje || 'Error al validar el email. Por favor, intenta de nuevo.';
+      }
+    });
+  }
+}
