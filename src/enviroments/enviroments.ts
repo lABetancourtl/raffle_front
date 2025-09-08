ec0593f17c1bf846c0a088c84a29d77894f9453a
Anderson Betancourt Arenas <111938729+lABetancourtl@users.noreply.github.com>
2025-09-07 23:41:14 -0500

    despliegue

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -1,7 +1,7 @@
 export const enviroments = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  //apiUrl: 'https://raffle-back-2.onrender.com/api',
+  //apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://raffle-back-2.onrender.com/api',
   instagramUrl: 'https://www.instagram.com',
   telegramUrl: 'https://www.instagram.com'
 };
