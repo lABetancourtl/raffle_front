c8987e6b4ad5202414ba7cbdc5ec2fb8bc9f21a2
Anderson Betancourt Arenas <111938729+lABetancourtl@users.noreply.github.com>
2025-10-22 14:19:19 -0500

    Cambios de Gabino

--- src/app/services/raffle.service.ts
+++ src/app/services/raffle.service.ts
@@ -7,63 +7,64 @@ import { enviroments } from '../../enviroments/enviroments';
   providedIn: 'root'
 })
 export class RaffleService {
-    private apiUrl = enviroments.apiUrl  //url del api creada de manera global
+  private apiUrl = enviroments.apiUrl; // URL del API global
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-getAllRaffles(): Observable<any[]> {
+  getAllRaffles(): Observable<any[]> {
     const token = localStorage.getItem('authToken');
     const headers = new HttpHeaders({
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
     });
 
     return this.http.get<any[]>(`${this.apiUrl}/raffle/allRifas`, { headers });
-}
+  }
 
-
-cambiarEstadoRifa(dto: { id: string; nuevoEstado: string }) {
-      const token = localStorage.getItem('authToken');
+  cambiarEstadoRifa(dto: { id: string; nuevoEstado: string }) {
+    const token = localStorage.getItem('authToken');
     const headers = new HttpHeaders({
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
     });
-  return this.http.patch<any>(`${this.apiUrl}/raffle/cambiarEstadoRifa`, dto, { headers });
-}
+    return this.http.patch<any>(`${this.apiUrl}/raffle/cambiarEstadoRifa`, dto, { headers });
+  }
 
+  obtenerClientePorNumero(raffleId: string, numero: string): Observable<any> {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
 
+    const params = new HttpParams()
+      .set('raffleId', raffleId)
+      .set('numero', numero);
 
-obtenerClientePorNumero(raffleId: string, numero: string): Observable<any> {
-  const token = localStorage.getItem('authToken');
-  const headers = new HttpHeaders({
-    Authorization: `Bearer ${token}`,
-    'Content-Type': 'application/json'
-  });
+    return this.http.get<any>(`${this.apiUrl}/raffle/clientePorNumero`, {
+      headers,
+      params
+    });
+  }
 
-  const params = new HttpParams()
-    .set('raffleId', raffleId)
-    .set('numero', numero);
+  // Llamado a endpoint desde el home (para clientes)
+  getRaffleActiva(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/raffle/activa`);
+  }
 
-  return this.http.get<any>(`${this.apiUrl}/raffle/clientePorNumero`, { 
-    headers, 
-    params 
-  });
-}
+  obtenerNumerosPorEmail(email: string): Observable<string[]> {
+    return this.http.get<string[]>(
+      `${this.apiUrl}/raffle/numerosPorEmail/soloNumeros?email=${email}`
+    );
+  }
 
-
-//Llamdo a endpoint desde el home (para clientes)
-getRaffleActiva(): Observable<any> {
-  return this.http.get<any>(`${this.apiUrl}/raffle/activa`);
-}
-
-obtenerNumerosPorEmail(email: string): Observable<string[]> {
-  return this.http.get<string[]>(`${this.apiUrl}/raffle/numerosPorEmail/soloNumeros?email=${email}`);
-}
-
-
-obtenerCantidadNumerosDisponibles(idRaffle: string): Observable<{ error: boolean, respuesta: number }> {
-  return this.http.get<{ error: boolean, respuesta: number }>(`${this.apiUrl}/purchase/cantidadNumerosDisponibles?idRaffle=${idRaffle}`);
-}
+  obtenerCantidadNumerosDisponibles(
+    idRaffle: string
+  ): Observable<{ error: boolean; respuesta: number }> {
+    return this.http.get<{ error: boolean; respuesta: number }>(
+      `${this.apiUrl}/purchase/cantidadNumerosDisponibles?idRaffle=${idRaffle}`
+    );
+  }
 
   crearPreferenciaPago(datosPago: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/mercadopago/crear-preferencia`, datosPago);
@@ -74,12 +75,38 @@ obtenerCantidadNumerosDisponibles(idRaffle: string): Observable<{ error: boolean
   }
 
   getOperacionesByRaffle(raffleId: string): Observable<any> {
-  const token = localStorage.getItem('authToken');
-  const headers = new HttpHeaders({
-    Authorization: `Bearer ${token}`,
-    'Content-Type': 'application/json'
-  });
-  return this.http.get<any>(`${this.apiUrl}/raffle/operaciones/${raffleId}`, { headers });
-}
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+    return this.http.get<any>(`${this.apiUrl}/raffle/operaciones/${raffleId}`, { headers });
+  }
+
+  // ✅ NUEVO MÉTODO para ejecutar el sorteo
+  ejecutarSorteo(raffleId: string, numeroGanadores: number): Observable<any> {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post<any>(
+      `${this.apiUrl}/raffle/ejecutarSorteo/${raffleId}?numeroGanadores=${numeroGanadores}`,
+      {},
+      { headers }
+    );
+  }
+
+  descargarActa(sorteoId: number) {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.get(`${this.apiUrl}/raffle/acta/${sorteoId}/pdf`, {
+      headers,
+      responseType: 'blob'
+    });
+  }
 
 }
